70700ead96453da05c48041cb422b9c0a2bbd56e
Jorge Luis Gomez Donis <j_donis@outlook.es>
2022-10-03 20:37:56 -0600

    .
    .

--- Tarea 5/quiniela/app/models/group.rb
+++ Tarea 5/quiniela/app/models/group.rb
@@ -1,2 +1,3 @@
 class Group < ApplicationRecord
+    belongs_to :teams, class_name: "teams", foreign_key: "teams_id"
 end
--- Tarea 5/quiniela/db/migrate/20220928051722_create_groups.rb
+++ Tarea 5/quiniela/db/migrate/20220928051722_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.bigint :team_id
+      t.bigint :teams_id
       t.string :group_name
 
       t.timestamps
